b8f3eeac7de3e695b0b2e157fbd50f9eae44238e
Tumurtogtokh <tdavaakhuu1@sheffield.ac.uk>
2019-01-27 20:32:21 +0000

    af

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,7 +1,12 @@
 #include "test_framework/generic_test.h"
 short Parity(unsigned long long x) {
   // TODO - you fill in here.
-  return 0;
+  short res = 0;
+  while(x){
+    res ^= (x&1);
+    x >>= 1;
+  }
+  return res;
 }
 
 int main(int argc, char* argv[]) {
